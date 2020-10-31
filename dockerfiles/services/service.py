ddf55b5d0214f5d3a8d8b5792e2b73973cb9087c
SemyonSinchenko <semyonsinchenko@yahoo.com>
2020-10-31 16:04:53 +0300

    fasds

--- dockerfiles/services/service.py
+++ dockerfiles/services/service.py
@@ -105,7 +105,7 @@ def load_from_db():
         id = int(request.args.get("id"))
         select_expr = select(columns=[images_table.c.image_data], from_obj=images_table, whereclause=images_table.c.image_id==id)
 
-        res = session.execure(select_expr).fetchall()
+        res = session.execute(select_expr).fetchall()
         img_stream = BytesIO(base64.decode(res[0][0]))
         img_stream.seek(0)
 
